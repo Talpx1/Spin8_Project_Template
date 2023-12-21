f3fcbc0a3d125a8e60ec7aaf7fca8e9d6a675f52
Talpx1 <simo3401@gmail.com>
2023-12-21 23:22:53 +0000

    updated and fixed versions in envionment config file

--- configs/environment.php
+++ configs/environment.php
@@ -3,7 +3,7 @@
 use Spin8\Configs\Enums\Environments;
 
 return [
-    'min_php_version' => 8.1,
-    'min_wordpress_version' => 6.0,
+    'min_php_version' => "8.3",
+    'min_wordpress_version' => "6.4.2",
     'environment' => Environments::LOCAL
 ];
