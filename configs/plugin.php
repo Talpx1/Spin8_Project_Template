b40265d65dabbd58a5c8aafd6f03441efc1af19f
Talpx1 <simo3401@gmail.com>
2023-12-25 23:20:43 +0100

    added the PLUGIN_SLUG env var

--- configs/plugin.php
+++ configs/plugin.php
@@ -1,6 +1,7 @@
 <?php
 return [
-    'name' => slugify(env("PLUGIN_NAME")), //should be in slug format, use the slugify function to be sure it's correct
+    'name' => env("PLUGIN_NAME"),
+    'slug' => env("PLUGIN_SLUG"), //should be in slug format, use the slugify function to be sure it's correct: slugify(env("PLUGIN_NAME"))
     'version' => '0.1',
     'namespace' => env("PLUGIN_NAMESPACE"),
 ];
