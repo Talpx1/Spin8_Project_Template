695ac81b237b5399a9a6cf5d81a8a31536069d03
Talpx1 <simo3401@gmail.com>
2024-01-03 00:54:30 +0100

    changed the plugin file name from spin8-dev to plugin-name so that it's more obvoius (will be replaced by the installation wizard anyway)

---
+++ plugin-name.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+/**
+ * $PLUGIN_NAME
+ *
+ * @package           %PLUGIN_NAMESPACE%
+ * @author            %PLUGIN_AUTHOR%
+ * @copyright         %YEAR% %PLUGIN_AUTHOR%
+ * @license           %PLUGIN_LICENSE%
+ *
+ * @wordpress-plugin
+ * Plugin Name:       %PLUGIN_NAME%
+ * Plugin URI:        %PLUGIN_URI%
+ * Description:       %PLUGIN_DESCRIPTION%
+ * Version:           %PLUGIN_VERSION%
+ * Requires at least: %MIN_WORDPRESS_VERSION%
+ * Requires PHP:      %MIN_PHP_VERSION%
+ * Author:            %PLUGIN_AUTHOR%
+ * Author URI:        %PLUGIN_AUTHOR_URI%
+ * License:           %PLUGIN_LICENSE%
+ * License URI:       %PLUGIN_LICENSE_URI%
+ * Text Domain:       %PLUGIN_SLUG%
+ * Domain Path:       /languages
+ * Update URI:        %PLUGIN_UPDATE_URI%
+ */
