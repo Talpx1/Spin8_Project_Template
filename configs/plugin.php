b26e7195193acd93e00b0dc6edaaedaa236ad561
Talpx1 <simo3401@gmail.com>
2024-01-04 22:04:25 +0100

    spin8 binary: fixed escaping of variables in install_command, fixed bash command + minor fixes and additions

--- configs/plugin.php
+++ configs/plugin.php
@@ -5,9 +5,10 @@ return [
     'slug' => envOr("PLUGIN_SLUG", slugify(env("PLUGIN_NAME"))), //should be in slug format
     'namespace' => env("PLUGIN_NAMESPACE"),
     'version' => '0.1',
+    'uri' => 'https://github.com/Talpx1/Spin8_Project_Template',
     'description' => 'An awesome plugin created with Spin8',
     'author' => 'Spin8',
-    'author_uri' => 'https://github.com/Talpx1',
+    'author_uri' => 'https://github.com/Talpx1/Spin8_Project_Template',
     'license' => 'MIT',
     'license_uri' => 'https://opensource.org/license/mit/',
     'update_uri' => '',
